4149e25844250f40e31e3434b573c264f948a641
CraigHollaman <7602163+CraigHollaman@users.noreply.github.com>
2022-11-10 12:54:04 +0000

    More Changes

--- QuotePreview.js
+++ QuotePreview.js
@@ -57,32 +57,6 @@ function CallWebService() {
 }
 
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function nextSlide(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slide");
-    let indicator = document.getElementsByClassName("indicator");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < indicator.length; i++) {
-        indicator[i].className = indicator[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    indicator[slideIndex - 1].className += " active";
-}
 
 function openSection(section, evt) {
     event.preventDefault()
